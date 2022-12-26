b5eca78c80962e876556ab8f74bb9992d7600c49
rpeduche <r.peduche@samsung.com>
2022-12-26 12:35:56 +0800

    # Calculator # Fix # Reset size of display value

--- src/components/AutoScale/index.js
+++ src/components/AutoScale/index.js
@@ -22,12 +22,13 @@ const AutoScale = ({ children, parentRef }) => {
   const scaleRef = useRef(null);
 
   const [scaleMemoValue, setScaleMemoValue] = useState(1);
-
   useEffect(() => {
     if (scaleRef?.current?.offsetWidth + 50 > parentRef?.current?.clientWidth) {
       setScaleMemoValue(
         parentRef?.current?.clientWidth / (scaleRef?.current.clientWidth + 100),
       );
+    } else {
+      setScaleMemoValue(1);
     }
   }, [children, scaleRef?.current?.offsetWidth, parentRef?.current?.offsetWidth]);
 
